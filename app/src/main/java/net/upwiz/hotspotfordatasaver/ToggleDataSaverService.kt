c47e98e819c69c65f31b9d73297a6f272971907e
esbang <esbang@mtn.co.kr>
2022-12-09 09:51:50 +0900

    #add. 기본 퀵메뉴 타일 추가

---
+++ app/src/main/java/net/upwiz/hotspotfordatasaver/ToggleDataSaverService.kt
@@ -0,0 +1,18 @@
+package net.upwiz.hotspotfordatasaver
+
+import android.service.quicksettings.TileService
+
+class ToggleDataSaverService : TileService() {
+    override fun onTileAdded() {
+        // Handle the tile being added to the Quick Settings panel
+    }
+
+    override fun onTileRemoved() {
+        // Handle the tile being removed from the Quick Settings panel
+    }
+
+    override fun onClick() {
+        // Handle the tile being clicked
+    }
+
+}
