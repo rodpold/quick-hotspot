f62497145cf18ebe6c353015e070d69904258bd8
esbang <esbang@mtn.co.kr>
2022-12-20 08:54:21 +0900

    #add. 버그 수정 및 ap state 가져오기

--- app/src/main/java/net/upwiz/hotspotfordatasaver/ToggleDataSaverService.kt
+++ app/src/main/java/net/upwiz/hotspotfordatasaver/ToggleDataSaverService.kt
@@ -1,10 +1,30 @@
 package net.upwiz.hotspotfordatasaver
 
+import android.content.Context
 import android.service.quicksettings.TileService
+import android.net.wifi.WifiManager
+import android.service.quicksettings.Tile
+import android.util.Log
+import java.lang.reflect.Method
 
 class ToggleDataSaverService : TileService() {
+
     override fun onTileAdded() {
+        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         // Handle the tile being added to the Quick Settings panel
+        val method: Method = wifiManager.javaClass.getMethod("getWifiApState")
+        method.isAccessible = true
+        val invoke = method.invoke(wifiManager)
+
+        if (invoke != null) {
+            Log.d("added invoke", invoke.toString())
+        }
+
+        if (invoke == 13) {
+            qsTile.state = Tile.STATE_ACTIVE
+        } else {
+            qsTile.state = Tile.STATE_INACTIVE
+        }
     }
 
     override fun onTileRemoved() {
@@ -13,6 +33,21 @@ class ToggleDataSaverService : TileService() {
 
     override fun onClick() {
         // Handle the tile being clicked
+        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val method: Method = wifiManager.javaClass.getMethod("getWifiApState")
+        method.isAccessible = true
+        val invoke = method.invoke(wifiManager)
+
+        // log invoke
+        if (invoke != null) {
+            Log.d("click invoke", invoke.toString())
+        }
+
+        if (invoke == 13) {
+            qsTile.state = Tile.STATE_ACTIVE
+        } else {
+            qsTile.state = Tile.STATE_INACTIVE
+        }
     }
 
 }
